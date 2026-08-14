3038460c4cc3110bab4f041bdb885d5b4340db65
kesavasankari <kesava.sankari@verinite.com>
2026-08-14 13:29:51 +0530

    feat(common): expose createdBy via getters on BaseEntity

--- common-lib/src/main/java/com/verinite/common/entity/BaseEntity.java
+++ common-lib/src/main/java/com/verinite/common/entity/BaseEntity.java
@@ -7,9 +7,10 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import lombok.Getter;
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
